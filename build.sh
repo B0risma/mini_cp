d2138b413d5e6ebe3146c390df3785d2925fb74a
BorisovMA <borisov@vipaks.ru>
2024-11-29 18:27:55 +0500

    update test code

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-g++ -o mini_cp main.cpp
+${CXX} -o mini_cp main.cpp
 chmod +x mini_cp
--- main.cpp
+++ main.cpp
@@ -10,10 +10,11 @@
 #include <unistd.h>
 #include <cstdint>
 #include <vector>
+#include "stdlib.h"
 
 //https://man7.org/linux/man-pages/man2/posix_fadvise.2.html
 
-constexpr auto size = 1024;
+int size = 512;
 
 int posix_write(const char* in, const char *out, const bool nobuf = false){
     int flag = O_CREAT | O_WRONLY;
@@ -46,22 +47,28 @@ int posix_write(const char* in, const char *out, const bool nobuf = false){
         return -1;
 }
 
-int c_write(const char* in, const char *out, const bool nobuf = false){
+int c_write(const char* in, const char *out, const bool usePosix = false,const bool nobuf = false){
     auto fin = fopen(in, "r");
     auto fout = fopen(out, "w");
-    auto outb = new char[size];
+    int fd = fileno(fout);
+//     auto outb = new char[size];
     bool ret = true;
     long fsize = 0;
     char* inb = 0;
+    
+    int partSize = size;
+    int bytes = 0;
+    int written = 0;
+    
     if(!fin || !fout){
         std::cout << "error open file\n";
         goto err;
     }
 
-    if(nobuf)
-        ret &= !bool(setvbuf(fout,0,_IONBF,0));
-    else 
-        ret &= !bool(setvbuf(fout,outb,0,size));
+//     if(nobuf)
+//         ret &= !bool(setvbuf(fout,0,_IONBF,0));
+//     else 
+//         ret &= !bool(setvbuf(fout,outb,0,size));
     if(!ret) goto err;
 
     fseek(fin, 0, SEEK_END);
@@ -73,22 +80,39 @@ int c_write(const char* in, const char *out, const bool nobuf = false){
         std::cout << "read error " << std::ferror(fin) << '\n';
         goto err; 
     }
+    if(nobuf){ //this make flash writing OK
+    while( bytes < fsize && partSize > 0){
+        if(!usePosix) written = std::fwrite(inb + bytes, 1, partSize, fout);
+        else written = write(fd, inb+bytes, partSize);
+        if(written > 0)
+            bytes+= written;
+        else {
+            std::cout << "write error\n";
+            goto err;
+        }
+//         printf("bytes %d part %d\n",bytes, partSize);
+        if((bytes + partSize) > fsize) partSize = fsize - bytes;
+        
+    }
+    }
+    else{
     ret &= std::fwrite(inb, fsize, 1, fout) != 0;
     if(!ret){
         std::cout << "write error\n";
         goto err; 
     }
+    }
     fclose(fin);
     std::fflush(fout);
     fclose(fout);
     delete [] inb;
-    delete [] outb;
+//     delete [] outb;
     return 0;
     err:
         fclose(fin);
         fclose(fout);
         delete [] inb;
-        delete [] outb;
+//         delete [] outb;
         return -1;
 }
 
@@ -102,7 +126,7 @@ int fstream_write(const char* in, const char *out, const bool nobuf = false){
     std::string buf;
     buf.resize(fin.tellg());
     fin.seekg(0);
-    fin.read(buf.data(), buf.size());
+    fin.read((char*)buf.data(), buf.size());
     fout.write(buf.data(), buf.size());
     fout.flush();
     return 0;
@@ -123,9 +147,14 @@ int main(int argc, const char ** argv){
             std::cout << "undefined option " << argv[3] << " " << argv[4] << '\n';
         }
     }
+    if(argc > 5){
+        size = atoi(argv[5]);
+        
+    }
     std::cout << "in " << in << '\n';
     std::cout << "out " << out << '\n';
     std::cout << "mode " << mode << '\n';
+    std::cout << "buffsize " << size << '\n';
     if(in.empty() || out.empty()) {
         std::cout << "empty filenames\n";
         return -2;
@@ -134,7 +163,11 @@ int main(int argc, const char ** argv){
     
     const bool nobuf = mode.find("nb") != std::string::npos;
     if(nobuf) std::cout << "without buffer\n";
-    if(mode.find('c') != std::string::npos){
+    if(mode.find("cp") != std::string::npos){
+        std::cout << "C with posix write\n";
+        return c_write(in.c_str(), out.c_str(),true, nobuf);
+    }
+    else if(mode.find('c') != std::string::npos){
         return c_write(in.c_str(), out.c_str(), nobuf);
     }
     else if(mode.find('f') != std::string::npos) {
